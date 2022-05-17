ddbdda3957d52ea11819472726125240106ffb64
FabioMartins <fabbio1000@gmail.com>
2022-05-16 21:49:55 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('test page About.js', () => {
+  it('testando o subtítulo da página "About Pokédex"', () => {
+    render(<About />);
+    const subTitulo = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(subTitulo).toBeInTheDocument();
+  });
+
+  it('testando primeiro parágrafo de informação', () => {
+    render(<About />);
+    const primeiroParagrafo = screen.getByText(/simulates a Pokédex, a digital/i);
+    expect(primeiroParagrafo).toBeInTheDocument();
+  });
+
+  it('testando segundo parágrafo de informação', () => {
+    render(<About />);
+    const segundoParagrafo = screen.getByText(/filter Pokémons by type, and see/i);
+    expect(segundoParagrafo).toBeInTheDocument();
+  });
+
+  it('testando se a imagem do aparelho aparece na tela', () => {
+    render(<About />);
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const imagem = screen.getByRole('img');
+    expect(imagem).toBeInTheDocument();
+    expect(imagem).toHaveAttribute('src', src);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,20 +4,20 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('test component App.js', () => {
-  test('deveria renderizar link Home', () => {
+  it('deveria renderizar link Home', () => {
     renderWithRouter(<App />);
     const linkHome = screen.getByRole('link', { name: 'Home' });
     expect(linkHome).toBeInTheDocument();
   });
 
-  test('deveria renderizar link About', () => {
+  it('deveria renderizar link About', () => {
     renderWithRouter(<App />);
 
     const linkAbout = screen.getByRole('link', { name: 'About' });
     expect(linkAbout).toBeInTheDocument();
   });
 
-  test.only('deveria renderizar link Favorite Pokémons', () => {
+  test('deveria renderizar link Favorite Pokémons', () => {
     renderWithRouter(<App />);
     const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
     expect(linkFavorite).toBeInTheDocument();
