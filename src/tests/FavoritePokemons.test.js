c9a5830d1a0f800d08abbf514925ba49515be913
FabioMartins <fabbio1000@gmail.com>
2022-05-16 23:11:48 -0300

    requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 
-describe('test page About.js', () => {
-  it('testando mensagem inicial da página vazia "Not found"', () => {
-    render(<FavoritePokemons />);
-    const notFound = screen.getByText(/No favorite pokemon found/i);
-    expect(notFound).toBeInTheDocument();
-  });
-
+describe('test page FavoritePokemons.js', () => {
   it('testando o subtítulo da página "Favorite pokémons"', () => {
     render(<FavoritePokemons />);
     const subTitulo = screen.getByRole('heading',
       { name: 'Favorite pokémons', level: 2 });
     expect(subTitulo).toBeInTheDocument();
+  });
+
+  it('testando mensagem inicial da página vazia "Not found"', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('test page NotFound.js', () => {
+  it('testando o subtítulo da página "Page requested not found 😭"', () => {
+    render(<NotFound />);
+    const subTitulo = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+    expect(subTitulo).toBeInTheDocument();
+
+    const emoji = screen.getByText('😭');
+    expect(emoji).toBeInTheDocument();
+  });
+
+  it('testando se a imagem aparece na tela', () => {
+    render(<NotFound />);
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imagem = screen.getAllByRole('img');
+    expect(imagem[1]).toBeInTheDocument();
+    expect(imagem[1]).toHaveAttribute('src', src);
+  });
+});
